9e05bcf04c0d9ec5b8e6fe6a06dbe2b7a21e372a
amina <amina.yahia@eniso.u-sousse.tn>
2024-01-13 18:18:42 +0100

    [FC] first commit containe simple xml list of file

--- src/main/java/com/safequery/back/projectdemo/contoller/NodeFile.java
+++ src/main/java/com/safequery/back/projectdemo/contoller/NodeFile.java
@@ -1,21 +1,13 @@
 package com.safequery.back.projectdemo.contoller;
 
-import ch.qos.logback.core.model.Model;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,10 +16,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -67,7 +57,6 @@ public class NodeFile {
 ////        }
 ////        return "folder";
 //    }
-private static String uploadedFolderPath;
 @PostMapping("/upload")
 public ResponseEntity<List<String>> uploadFile(@RequestParam("file") List<MultipartFile> files,@RequestParam(required = false) String folderPath ,  HttpSession session) {
     try {
